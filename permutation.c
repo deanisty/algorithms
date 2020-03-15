62109aa7246359fcd8672d224a6c30c4702e3a2f
Caoyu <353238473@qq.com>
2020-03-15 16:22:44 +0800

    prev_permutation

--- permutation.c
+++ permutation.c
@@ -9,16 +9,19 @@ permutation(全排列)
 2. 此时，在 S[i+1] 一直到字符串末尾的位置查找所有比 S[i] 大的字符中的最小的字符，假设该字符为 S[min]，然后交换 S[i] 和 S[min] 的位置
    (此时涵盖 S[i+1] 就是字符串末尾字符的情况，即找到的字符就是 S[i+1])
 3. 最后将 S[i+1] 以及直至字符串末尾的位置逆序，这样得到的 S 就是下一个排列
+同理，prev_permutation 函数负责找到输入字符串的上一个排列，其逻辑也是从字符串末尾开始查找，但是操作逻辑和 next_permutation 完全相反
 ****/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
+bool prev_permutation(char s[], int length);
 bool next_permutation(char s[], int length);
 bool reverse(char s[], int start, int end);
 bool swap(char s[], int i, int j);
 int get_min_greater(char s[], char c, int start, int end);
+int get_max_lesser(char s[], char c, int start, int end);
 
 int main(int argc, char *argv[])
 {
@@ -35,13 +38,18 @@ int main(int argc, char *argv[])
     // test get_min_greater
     // printf("min is %d\n", get_min_greater(str, 'f', 0, 12));
 
-    int total = 0;
-    // 如果此时的字符串不是按照字典序排列，则可以选择先将其按字段序升序排列
-    // 当然，还有一个选择就是另写一个 prev_permutation 方法用来寻找当前排列的上一个排列，也参考 C++ STL 中的 prev_permutation 方法
+    int total = 1; // 当前字符串本身也是一个排列
+    // 找到当前字符串的下一个排列字符串
     while(next_permutation(str, length)) { // 当字符串已经全部逆序排列时 终止循环 已经找到所有排列
         total++;
         printf("%s\n", str);
     }
+    // 示例字符串是完全升序字符串，所以运行到这里已经是遍历到了所有的排列了
+    // 如果字符串并不是完全升序排列的话，可以把当前字符串复制一份，然后执行 prev_permutation 函数，依次找到它前面的所有排列
+    // while(prev_permutation(str, length)) {
+    //    total++;
+    //    printf("%s\n", str);
+    // }
 
     printf("total is %d\n", total); // 总共有多少个排列
 
@@ -61,6 +69,40 @@ bool next_permutation(char s[], int length)
     }
 
     return false;
+}
+
+// 寻找字符串s的前面的排列，直至所有字符完全降序
+bool prev_permutation(char s[], int length)
+{
+    int i, max;
+    for(i = length - 1; i > 0; i--) {
+        if(s[i-1] > s[i]) {
+            max = get_max_lesser(s, s[i-1], i, length - 1);
+            swap(s, i-1, max);
+            reverse(s, i, length - 1);
+            return true;
+        }
+    }
+
+    return false;
+}
+// 找到字符串s中从start到end中的所有比c小的字符的最大的那个，返回字符的位置，如果没找到返回 -1
+int get_max_lesser(char s[], char c, int start, int end)
+{
+    int i;
+    int max = -1; // not found
+    for(i = start; i <= end; i++) {
+        if(s[i] < c) {
+            if(max == -1) {
+                max = i;
+            }
+            if(s[i] > s[max]) {
+                max = i;
+            }
+        }
+    }
+
+    return max;
 }
 // 找到字符串s中从start到end中的所有比c大的字符的最小的那个，返回字符的位置，如果没找到返回 -1
 int get_min_greater(char s[], char c, int start, int end)
